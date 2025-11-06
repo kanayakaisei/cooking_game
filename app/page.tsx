f4198b9fcb1d5daa3a159b3909460d498dfb509f
kanayakaisei <kanayakaisei0903@gmail.com>
2025-11-06 19:49:38 +0900

    クラウドフレアテスト

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>トップページ</h1>
+      <p className={styles.explanation}>子供が遊ぶ料理ゲーム</p>
     </div>
   );
 }
