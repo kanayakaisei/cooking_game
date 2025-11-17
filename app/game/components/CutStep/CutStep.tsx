e13353aa30b635b93b482c88fb9fb952c7cdf54a
Yamagata Kaito <yamagatakaito0921@gmail.com>
2025-11-17 17:47:44 +0900

    style_ゲーム画面

--- app/game/components/CutStep/CutStep.tsx
+++ app/game/components/CutStep/CutStep.tsx
@@ -4,18 +4,39 @@ import Image from "next/image";
 const Cut = () => {
     return (
         <>
-            <Image
-                src="/image/Ellipse.png"
-                width={400}
-                height={300}
-                alt="まないた"
-            ></Image >
-            <Image
-                src="/image/Group.png"
-                width={200}
-                height={360}
-                alt="包丁"
-            ></Image>
+            <div className={styles.Wrapper}>
+                <h1 className={styles.title}>肉じゃが</h1>
+                <div className={styles.imageWrap}>
+                    <Image
+                        src="/image/chara.png"
+                        width={200}
+                        height={214}
+                        alt="キャラクター"
+                        className={styles.chara}
+                    ></Image >
+                    <Image
+                        src="/image/Group78.png"
+                        width={840}
+                        height={280}
+                        alt="まないた"
+                        className={styles.cutBoard}
+                    ></Image >
+                    <Image
+                        src="/image/potato.png"
+                        width={450}
+                        height={250}
+                        alt="じゃがいも"
+                        className={styles.potato}
+                    ></Image >
+                    <Image
+                        src="/image/knife.png"
+                        width={220}
+                        height={420}
+                        alt="包丁"
+                        className={styles.knife}
+                    ></Image>
+                </div>
+            </div>
         </>
     )
 }
