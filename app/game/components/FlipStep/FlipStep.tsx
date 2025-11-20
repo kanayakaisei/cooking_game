4ec6864329fbad7707554a801ed6b8755df871f0
Yamagata Kaito <yamagatakaito0921@gmail.com>
2025-11-20 13:26:19 +0900

    feat_ゲーム混ぜる画面

--- app/game/components/FlipStep/FlipStep.tsx
+++ app/game/components/FlipStep/FlipStep.tsx
@@ -1,10 +1,19 @@
 import styles from "./flipStep.module.css";
+import Image from "next/image";
+
 
 const Flip = () => {
     return (
-        <div>
-            <h1>食材をひっくり返す</h1>
-        </div>
+        <>
+
+
+            <Image
+                src="/image/frypan.png"
+                width={260}
+                height={400}
+                alt="フライパン"
+            ></Image>
+        </>
     )
 }
 
--- app/game/components/MixStep/MixStep.tsx
+++ app/game/components/MixStep/MixStep.tsx
@@ -1,10 +1,34 @@
 import styles from "./mixStep.module.css";
+import Image from "next/image";
 
 const Mix = () => {
     return (
-        <div>
-            <h1>料理を混ぜる</h1>
-        </div>
+        <>
+
+            <div className={styles.imageWrap}>
+                <Image
+                    src="/image/chara.png"
+                    width={200}
+                    height={214}
+                    alt="キャラクター"
+                    className={styles.chara}
+                ></Image >
+                <Image
+                    src="/image/pot.png"
+                    width={540}
+                    height={330}
+                    alt="なべ"
+                    className={styles.pot}
+                ></Image >
+                <Image
+                    src="/image/ladle.png"
+                    width={180}
+                    height={330}
+                    alt="おたま"
+                    className={styles.ladle}
+                ></Image>
+            </div>
+        </>
     )
 }
 
