2e3c946a66a55381d2abc629edc3dd33c1261044
Yamagata Kaito <yamagatakaito0921@gmail.com>
2025-11-20 12:27:30 +0900

    feat_ゲーム画面

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -1,16 +1,67 @@
 "use client";
 import { useState } from "react";
 import styles from "./page.module.css";
+import Image from "next/image";
+import Link from "next/link";
 import Cut from "./components/CutStep/CutStep";
 import Mix from "./components/MixStep/MixStep";
 import Flip from "./components/FlipStep/FlipStep";
 
 
+
 const Game = () => {
+
+    const [step, setStep] = useState("cut");
+    const [count, setCount] = useState(0)
+
+    const newCount = count + 1;
+
+    const handle = () => {
+        if (step === "cut") {
+            setStep("mix");
+        } else if (step === "mix") {
+            setCount(newCount);
+            if (newCount >= 10) {
+                setStep("flip");
+            }
+        } else if (step === "flip") {
+            setStep("finish");
+        } else if (step === "finish") {
+            setStep("cut")
+        }
+    };
+
+
     return (
-        <div>
-            <h1>ゲーム本体ページ（流れの管理）</h1>
-        </div>
+        <>
+            <div className={styles.mainVisual}>
+                <div className={styles.Wrapper}>
+                    <div className={styles.titleWrap}>
+                        <button className={styles.circle}>
+                            <Image
+                                src="/image/arrow.png"
+                                width={80}
+                                height={80}
+                                alt="矢印"
+                                className={styles.arrow}
+                            />
+                        </button>
+                        <h1 className={styles.title}>肉じゃが</h1>
+                    </div>
+                    <div>
+                        {step === "cut" && <Cut />}
+                        {step === "mix" && <Mix />}
+                        {step === "flip" && <Flip />}
+                        {step === "finish" && <p>ゲーム完了！ (成功と失敗のページ切り替えをここで出来てるように) <Link href="/">戻る</Link > </p>}
+
+                        {step !== "finish" && (
+                            <button onClick={handle} className={styles.click}>クリック回数{count}</button>
+                        )}
+                    </div>
+                </div>
+            </div >
+
+        </>
     )
 }
 
