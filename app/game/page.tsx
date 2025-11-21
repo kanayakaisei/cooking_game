a0e71d348c153a5ff82beaf6cd4b3dd46cffb0e8
Yamagata Kaito <yamagatakaito0921@gmail.com>
2025-11-22 07:37:22 +0900

    feat_野菜の画像入れ替わり

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -16,6 +16,12 @@ type Step =
     | "carrotMsg"
     | "cutCarrotMsg"
     | "carrotCut"
+    | "onionCutMsg"
+    | "cutOnionMsg"
+    | "onionCut"
+    | "meatCutMsg"
+    | "cutMeatMsg"
+    | "meatCut"
     | "mixMsg"
     | "mix"
     | "flipMsg"
@@ -29,8 +35,18 @@ const potatoImages = [
 ];
 
 const carrotImages = [
-    "/image/carrots.png",
-    "/image/cutCarrots.png",
+    "/image/carrot.png",
+    "/image/cutCarrot.png",
+];
+
+const meatImages = [
+    "/image/meat.png",
+    "/image/cutMeat.png",
+];
+
+const onionImages = [
+    "/image/onion.png",
+    "/image/cutOnion.png",
 ];
 
 const Game = () => {
@@ -49,18 +65,49 @@ const Game = () => {
 
 
         if (step === "potatoMsg") {
-            setStep("cut");
-        } else if (step === "cut") {
+            setStep("cutPotatoMsg");
+        } else if (step === "cutPotatoMsg") {
+            setStep("potatoCut");
+            //じゃがいも
+        } else if (step === "potatoCut") {
+            setStep("carrotMsg");
+            //にんじん
+        } else if (step === "carrotMsg") {
+            setStep("cutCarrotMsg");
+        } else if (step === "cutCarrotMsg") {
+            setStep("carrotCut");
+        } else if (step === "carrotCut") {
+            setStep("meatCutMsg");
+            //お肉
+        } else if (step === "cutMeatMsg") {
+            setStep("meatCut");
+        } else if (step === "meatCut") {
+            setStep("onionMsg");
+            //タマネギ
+        } else if (step === "cutOnionMsg") {
+            setStep("onionCut");
+
+            //混ぜる画面
+        } else if (step === "potatoCut") {
+            setStep("mixMsg");
+        } else if (step === "mixMsg") {
             setStep("mix");
+            setMixCount(0);
         } else if (step === "mix") {
             setCount(newCount);
             if (newCount >= 10) {
-                setStep("flip");
+                setStep("flipMsg");
             }
-        } else if (step === "flip") {
+
+            //ひっくり返す画面
+        } else if (step === "flipMsg") {
+            setStep("flip");
+        }
+        else if (step === "flip") {
             setStep("finish");
-        } else if (step === "finish") {
-            setStep("cut")
+        }
+        else if (step === "finish") {
+            return;
         }
     };
 
@@ -98,7 +145,7 @@ const Game = () => {
                             alt="じゃがいもの料理写真"
                         />}
 
-                        {step === "message" && <Message
+                        {step === "cutPotatoMsg" && <Message
                             title="じゃがいもをきろう！"
                             src="/image/Me_cutPotato.png"
                             width={400}
@@ -106,15 +153,96 @@ const Game = () => {
                             alt="じゃがいもが半分の料理写真"
                         />}
 
-                        {step === "cut" && <Cut
+                        {step === "potatoCut" && <Cut
                             src="/image/potato.png"
                             width={450}
                             height={250}
                             alt="じゃがいも"
                         />}
 
-                        {step === "mix" && <Mix />}
-                        {step === "flip" && <Flip />}
+                        {step === "carrotMsg" && <Message
+                            title="にんじんをきろう！"
+                            src="/image/Me_potato.png"
+                            width={400}
+                            height={190}
+                            alt="にんじんの料理写真"
+                        />}
+
+                        {step === "cutCarrotMsg" && <Message
+                            title="にんじんをきろう！"
+                            src="/image/Me_cutPotato.png"
+                            width={400}
+                            height={190}
+                            alt="にんじんが半分の料理写真"
+                        />}
+
+                        {step === "carrotCut" && <Cut
+                            src="/image/carrot.png"
+                            width={450}
+                            height={250}
+                            alt="にんじん"
+                        />}
+
+                        {/* お肉 */}
+                        {step === "meatMsg" && <Message
+                            title="お肉をきろう！"
+                            src="/image/Me_potato.png"
+                            width={400}
+                            height={190}
+                            alt="お肉の料理写真"
+                        />}
+
+                        {step === "cutMeatMsg" && <Message
+                            title="お肉をきろう！"
+                            src="/image/Me_cutPotato.png"
+                            width={400}
+                            height={190}
+                            alt="お肉が半分の料理写真"
+                        />}
+
+                        {step === "meatCut" && <Cut
+                            src="/image/meat.png"
+                            width={450}
+                            height={250}
+                            alt="お肉"
+                        />}
+                        {/* たまねぎ */}
+                        {step === "onionMsg" && <Message
+                            title="たまねぎをきろう！"
+                            src="/image/Me_potato.png"
+                            width={400}
+                            height={190}
+                            alt="じゃがいもの料理写真"
+                        />}
+
+                        {step === "cutOnionMsg" && <Message
+                            title="たまねぎをきろう！"
+                            src="/image/Me_cutPotato.png"
+                            width={400}
+                            height={190}
+                            alt="たまねぎが半分の料理写真"
+                        />}
+
+                        {step === "onionCut" && <Cut
+                            src="/image/onion.png"
+                            width={450}
+                            height={250}
+                            alt="たまねぎ"
+                        />}
+
+                        {step === "mixMsg" && <Mix
+
+
+                        />}
+
+                        {step === "mix" && <Mix
+
+                        />}
+
+                        {step === "flipMsg" && <Flip
+
+                        />}
+
                         {step === "finish" && <p>ゲーム完了！ (成功と失敗のページ切り替えをここで出来てるように) <Link href="/">戻る</Link > </p>}
 
                         {step !== "finish" && (
