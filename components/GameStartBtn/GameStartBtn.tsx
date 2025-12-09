a4f046e5afc6bcf9148b94789f8e248eea0e3573
Yamagata Kaito <yamagatakaito0921@gmail.com>
2025-12-09 14:25:39 +0900

    create_ゲームスタートボタンコンポーネント作成

---
+++ components/GameStartBtn/GameStartBtn.tsx
@@ -0,0 +1,16 @@
+"use client";
+import { useState, useEffect } from "react";
+import styles from "./GameStartBtn.module.css";
+import Image from "next/image";
+import Link from "next/link";
+
+
+const GameStartBtn = () => {
+    return (
+        <>
+
+        </>
+    )
+}
+
+export default GameStartBtn;
