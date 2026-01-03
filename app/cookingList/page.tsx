5029f5cfb7db6be2ebc629101b8807b6b4bce010
kanayakaisei <kanayakaisei0903@gmail.com>
2026-01-04 01:16:05 +0900

    add_レイアウト完了

--- app/cookingList/page.tsx
+++ app/cookingList/page.tsx
@@ -1,7 +1,38 @@
+import styles from "./page.module.css";
+import Image from "next/image";
+import Heading from "@/components/Heading/Heading";
+import List from "@/lib/cookingList";
+
 const Select = () => {
     return (
         <div>
-            <h1>料理一覧画面</h1>
+            <Heading text="レシピいちらん" />
+            <div className={styles.cardWrap}>
+                {List.map((list) => (
+                    <article key={list.id} className={styles.card}>
+                        <h3>{list.title}</h3>
+                        <div className={styles.imageWrap}>
+                            <Image
+                                src={list.image}
+                                alt={list.title}
+                                width={250}
+                                height={180}
+                            />
+                        </div>
+                        <div className={styles.ingredients}>
+                            {list.ingredients.map((item) => (
+                                <Image
+                                    key={item}
+                                    src={item}
+                                    alt="ingredient"
+                                    width={60}
+                                    height={40}
+                                />
+                            ))}
+                        </div>
+                    </article>
+                ))}
+            </div>
         </div>
     )
 }
