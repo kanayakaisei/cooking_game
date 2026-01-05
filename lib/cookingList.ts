59b8355bc5a5d5704cda96aedd70b9d88f581f77
kanayakaisei <kanayakaisei0903@gmail.com>
2026-01-06 00:26:19 +0900

    chore_imageディレクトリページごとのimg分別

--- lib/cookingList.ts
+++ lib/cookingList.ts
@@ -2,27 +2,27 @@ const cookingList = [
     {
         id:1,
         title: "切る",
-        image: "/image/list_cut.svg",
+        image: "/image/cookingList/list_cut.svg",
         ingredients: []
     },
     {
         id:2,
         title: "まぜる",
-        image: "/image/list_cut.svg",
+        image: "/image/cookingList/list_cut.svg",
         ingredients: []
     },
     {
         id:3,
         title: "ひっくり返す",
-        image: "/image/list_cut.svg",
+        image: "/image/cookingList/list_cut.svg",
         ingredients: []
     },
     {
         id:4,
         title: "肉じゃが",
-        image: "/image/list_cut.svg",
+        image: "/image/cookingList/list_cut.svg",
         ingredients: [
-            "/image/meat_1.png"
+            
         ]
     },
 ]
