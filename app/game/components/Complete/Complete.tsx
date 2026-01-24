4fd46fd1c8b180a260cea8af106bd255d22164e8
kanayakaisei <kanayakaisei0903@gmail.com>
2026-01-24 19:47:09 +0900

    fix_エラー修正

--- app/game/components/Complete/Complete.tsx
+++ app/game/components/Complete/Complete.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import styles from "./complete.module.css";
 import Image from "next/image";
 import Link from "next/link";
