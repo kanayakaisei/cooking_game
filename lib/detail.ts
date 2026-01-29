0c0655b7d7633b495a6de511b4224c159dce217e
Yamagata Kaito <yamagatakaito0921@gmail.com>
2026-01-30 03:03:18 +0900

    chore_cookingListの写真差し替え

--- lib/detail.ts
+++ lib/detail.ts
@@ -1,26 +1,26 @@
 const cookingList = [
     {
-        id:1,
+        id: 1,
         title: "「切る」",
         image: "/image/cookingList/list_cut.svg",
         ingredients: []
     },
     {
-        id:2,
+        id: 2,
         title: "「まぜる」",
-        image: "/image/cookingList/list_cut.svg",
+        image: "/image/cookingList/list_mix.svg",
         ingredients: []
     },
     {
-        id:3,
+        id: 3,
         title: "「ひっくり返す」",
-        image: "/image/cookingList/list_cut.svg",
+        image: "/image/cookingList/list_flip.svg",
         ingredients: []
     },
     {
-        id:4,
+        id: 4,
         title: "肉じゃが",
-        image: "/image/cookingList/list_cut.svg",
+        image: "/image/cookingList/meat_potato.svg",
         ingredients: [
             "/image/cookingList/onion.svg",
             "/image/cookingList/potato.svg",
@@ -28,7 +28,7 @@ const cookingList = [
             "/image/cookingList/meat.svg"
         ]
     },
-    
+
 ]
 
 export default cookingList;
