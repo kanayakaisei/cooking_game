90ad89483df5fc2c52644eb20ac7e9cadd6f1f76
kanayakaisei <kanayakaisei0903@gmail.com>
2026-02-05 14:23:56 +0900

    fix_ビルドバグ修正

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -1,4 +1,5 @@
 "use client"
+export const dynamic = "force-dynamic";
 import { useState, useEffect, useMemo } from "react";
 import { useSearchParams } from "next/navigation";
 import styles from "./page.module.css";
